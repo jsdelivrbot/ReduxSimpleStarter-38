210117ca486f96eab95c1da7c5d918494a6ce173
Tin Chu <mchu.tinho@gmail.com>
2017-10-25 19:51:41 -0500

    video list item functional component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
-const VideoList = () => {
+const VideoList = ( props ) => {
+
+	const VideoItems = props.videos.map((video) => {
+		return <VideoListItem video={ video } />
+	});
+
 	return (
-		<ul>
-
+		<ul className="col-md-4 list-group">
+			{ VideoItems }
 		</ul>
 	);
 };
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM  from 'react-dom';
 import YTSearch  from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
 const API_KEY = 'AIzaSyC5_rcTVEs7BdJEb6GpexTurguRBjuhfO8';
 
 
@@ -21,6 +23,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={ this.state.videos } />
 			</div>
 		);
 	}
